8ef87973d8fc3540e6e4b48590a69ef40e42a760
Luca Vignali <lucavigno2003@gmail.com>
2025-08-06 17:16:10 +0200

    added missing pages and style

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import OrdiniView from '../views/OrdiniView.vue';
+import CategoriaView from '@/views/CategoriaView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'home', component: HomeView },
-    { path: '/ordini', name: 'ordini', component: OrdiniView }
+    { path: '/ordini', name: 'ordini', component: OrdiniView },
+    { path: '/categoria', name: 'categoria', component: CategoriaView },
+    { path: '/carrello', name: 'carrello', component: CarrelloView.vue },
+    {path: '/ricerca', name: 'ricerca', component: RicercaView.vue}
   ]
 });
 
