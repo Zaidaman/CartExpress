fdbe535f0ffd91058d66f12531515bcd11c4383b
Luca Vignali <lucavigno2003@gmail.com>
2025-08-18 18:22:31 +0200

    new style file

--- frontend/src/views/CategoriaView.vue
+++ frontend/src/views/CategoriaView.vue
@@ -29,37 +29,38 @@ async function caricaProdotti(idCategoria) {
 </script>
 
 <template>
-	<div class="view-container">
-		<div class="categorie-prodotti-wrapper">
-			<div class="categorie-container">
-				<h2>Categorie</h2>
-				<ul v-if="categorie.length">
-					<li v-for="cat in categorie" :key="cat.IdCategoria">
-						<button @click="caricaProdotti(cat.IdCategoria)">
-							<img :src="`/${cat.Immagine}`" alt="" />
-							<span>{{ cat.Nome }}</span>
-						</button>
+	<div class="categorie-prodotti-wrapper">
+		<div class="categorie-container">
+			<h2>Categorie</h2>
+			<ul v-if="categorie.length">
+				<li v-for="cat in categorie" :key="cat.IdCategoria">
+					<button @click="caricaProdotti(cat.IdCategoria)">
+						<img :src="`/${cat.Immagine}`" alt="" />
+						<span>{{ cat.Nome }}</span>
+					</button>
+				</li>
+			</ul>
+			<p v-else>Caricamento categorie...</p>
+		</div>
+		<div class="prodotti-container">
+			<div v-if="!categoriaSelezionata" class="placeholder">
+				<p>Seleziona una categoria per vedere i prodotti</p>
+			</div>
+			<div v-else>
+				<h2>Prodotti</h2>
+				<ul v-if="prodotti.length">
+					<li v-for="prod in prodotti" :key="prod.IdProdotto">
+						<img :src="`/${prod.Immagine}`" alt="" />
+						{{ prod.Nome }} - Prezzo: {{ prod.Prezzo }} €
 					</li>
 				</ul>
-				<p v-else>Caricamento categorie...</p>
-			</div>
-
-			<div class="prodotti-container">
-				<div v-if="!categoriaSelezionata" class="placeholder">
-					<p>Seleziona una categoria per vedere i prodotti</p>
-				</div>
-
-				<div v-else>
-					<h2>Prodotti</h2>
-					<ul v-if="prodotti.length">
-						<li v-for="prod in prodotti" :key="prod.IdProdotto">
-							<img :src="`/${prod.Immagine}`" alt="" />
-							{{ prod.Nome }} - Prezzo: {{ prod.Prezzo }} €
-						</li>
-					</ul>
-					<p v-else>Nessun prodotto trovato per questa categoria.</p>
-				</div>
+				<p v-else>Nessun prodotto trovato per questa categoria.</p>
 			</div>
 		</div>
 	</div>
 </template>
+
+
+<style lang="scss" scoped>
+@import '../styles/categoria.scss';
+</style>
