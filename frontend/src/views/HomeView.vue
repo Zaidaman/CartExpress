efade98b8fe38812c55aaffd2e483d1b46082c12
Zaidaman <cappella.filippo.it@gmail.com>
2025-09-01 19:10:37 +0200

    CSS MOD

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,11 +1,16 @@
-
 <template>
   <div class="view-container">
     <h1>Benvenuto a CartExpress</h1>
+    <h2>La tua spesa inizia da qui!</h2>
+    <button @click="vaiAllaCategoria">Vai alle Categorie</button>
   </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
 const router = useRouter();
+
+function vaiAllaCategoria() {
+  router.push('/categoria');
+}
 </script>
